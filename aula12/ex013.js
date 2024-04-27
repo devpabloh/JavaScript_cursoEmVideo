83a61170941345355137d7c84876b6644fc6452d
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-27 20:34:17 -0300

    Create ex013.js

---
+++ aula12/ex013.js
@@ -0,0 +1,8 @@
+let idade = 12
+if(idade < 16){
+    console.log('Não vota')
+}else if( idade >= 16 && idade < 18){
+    console.log('Votar é opcional')
+} else{
+    console.log('Votar é obrigatório')
+}

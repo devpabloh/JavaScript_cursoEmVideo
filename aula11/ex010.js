727f004f91a794e1d3f44a5bf979256d63e17d3c
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-27 09:10:24 -0300

    atualização

---
+++ aula11/ex010.js
@@ -0,0 +1,8 @@
+let vel = 60
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+
+if(vel > 60){
+    console.log(`Você ultrapassou o limite da via que é de 60KM/h, logo você será mutado!`)
+} else{
+    console.log('Você está dentro do limite de velocidade permitido, parabéns, continue assim!')
+}

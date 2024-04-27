e39bf122fdeef46b3fb245a2b20537975bddb1de
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-27 20:36:31 -0300

    atualização

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,7 +1,7 @@
-let idade = 12
+let idade = 70
 if(idade < 16){
     console.log('Não vota')
-}else if( idade >= 16 && idade < 18){
+}else if( idade < 18 || idade > 65){
     console.log('Votar é opcional')
 } else{
     console.log('Votar é obrigatório')

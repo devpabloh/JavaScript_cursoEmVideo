265dfecd678a2770490dc304fe933db6cb95da87
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-28 16:09:08 -0300

    atualização

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -3,14 +3,20 @@ function carregar(){
     let img = window.document.getElementById('imagem')
     let data = new Date()
     let horarioAtual = data.getHours()
+    horarioAtual = 20
+
     msg.innerHTML = `Agora são ${horarioAtual} horas.`
 
-    if(horarioAtual > 0 && horarioAtual < 12){
+    if(horarioAtual >= 0 && horarioAtual < 12){
         img.src = '../aula12ex/imagens/manhã.png'
-    }else if(horarioAtual < 12 && horarioAtual < 18){
+        document.body.style.background = '#FFFF00'
+    }else if(horarioAtual >= 12 && horarioAtual < 18){
         img.src = '../aula12ex/imagens/tarde.png'
+        document.body.style.background = '#B8860B'
     }else{
         img.src = '../aula12ex/imagens/pexels-rbrigant44-771883.png'
+        document.body.style.background = '#4F4F4F'
     }
 }
+
 

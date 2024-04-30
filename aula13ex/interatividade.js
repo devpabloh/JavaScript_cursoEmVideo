afd1a626ef717fbe88cea8dd3844630dbd02e308
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-30 09:26:26 -0300

    atualização

--- aula13ex/interatividade.js
+++ aula13ex/interatividade.js
@@ -13,11 +13,29 @@ function verificar(){
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 12){
+                img.src = ''
+            }else if(idade > 12 && idade < 18){
+                img.src = ''
+            }else{
+                img.src = ''
+            }
+
         }else if(fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 12){
+                img.src = ''
+            }else if(idade > 12 && idade < 18){
+                img.src = ''
+            }else{
+                img.src = ''
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }

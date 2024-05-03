2d7a30f038cd6774f2c07a7de211d243b911655d
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-03 19:01:35 -0300

    atualização

--- aula18/analisador.js
+++ aula18/analisador.js
@@ -1,7 +1,7 @@
 let num = document.getElementById('txtn') //recebe o número digitado de 1 a 100
 let lista = document.getElementById('flista') // lista de números para selecionar
 let res = document.getElementById('res') // resultado
-let vetores = []
+let valores = []
 
 function isNumero(n){
     if(Number(n) >= 1 && Number(n) <= 100){
@@ -12,7 +12,7 @@ function isNumero(n){
 }
 
 function inLista(n, l){
-    if(l.indexOf(Number(n))!= -1){
+    if ( l.indexOf(Number(n))   != -1){
         return true
     }else{
         return false
@@ -21,8 +21,24 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert('tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else{
         window.alert(`Valor invalido ou já foi encontrado na lista.`)
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    }else{
+        let tot = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados </p>`
     }
 }

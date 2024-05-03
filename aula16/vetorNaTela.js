78e2f0133f95d32a3e144d2d7feb20ccecdcc8da
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-03 10:26:08 -0300

    Update vetorNaTela.js

--- aula16/vetorNaTela.js
+++ aula16/vetorNaTela.js
@@ -1,5 +1,11 @@
 let valores = [8,1,7,4,2,9]
-valores.sort()
+
+let procurar = valores.indexOf(7) //serve para buscar valores dentro do arrray
+console.log(procurar)
+
+valores.sort() //serve para organizar em ordem crescente os valores do Array
+
+
 /* console.log(valores)
 console.log(valores[0])
 console.log(valores[1])
@@ -12,6 +18,7 @@ console.log(valores[5]) */
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 } */
 
+/* Usado para percorrer e mostrar o array de forma mais fácil
 for(let pos in valores){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
+} */

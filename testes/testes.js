81a72c1567ffde214c3c0a0a3c56c08f150270c3
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-12 17:44:52 -0300

    att

--- testes/testes.js
+++ testes/testes.js
@@ -1,17 +1,2 @@
 
 
-function contar(){
-    
-    let img = document.getElementsByClassName('img');
-
-    let srcAtual = img.getAttribute('src');
-
-    if(srcAtual === "imagens/Black Minimal Motivation Quote LinkedIn Banner.png"){
-        img.setAttribute('src', '/testes/imagens/icons8-react-native-500.png')
-
-    }else{
-        img.setAttribute('src', '')
-    }
-
-
-}
